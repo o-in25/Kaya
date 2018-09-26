56271eb19f7b360cc23c5943b70f22c0d2754b60
Eoin Halligan <ehalligan12@gmail.com>
2018-09-26 04:15:34 -0400

    commited the test file

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -2,9 +2,9 @@
  *
  *	Test program for the modules ASL and pcbQueues (phase 1).
  *
- *	Produces progress messages on terminal 0 in addition 
+ *	Produces progress messages on terminal 0 in addition
  *		to the array ``okbuf[]''
- *		Error messages will also appear on terminal 0 in 
+ *		Error messages will also appear on terminal 0 in
  *		addition to the array ``errbuf[]''.
  *
  *		Aborts as soon as an error is detected.
@@ -41,12 +41,12 @@ char *mp = okbuf;
 
 typedef unsigned int devreg;
 
-/* This function returns the terminal transmitter status value given its address */ 
+/* This function returns the terminal transmitter status value given its address */
 devreg termstat(memaddr * stataddr) {
 	return((*stataddr) & STATUSMASK);
 }
 
-/* This function prints a string on specified terminal and returns TRUE if 
+/* This function prints a string on specified terminal and returns TRUE if
  * print was successful, FALSE if not   */
 unsigned int termprint(char * str, unsigned int term) {
 	memaddr * statusp;
@@ -54,18 +54,18 @@ unsigned int termprint(char * str, unsigned int term) {
 	devreg stat;
 	devreg cmd;
 	unsigned int error = FALSE;
-	
+
 	if (term < DEVPERINT) {
 		/* terminal is correct */
 		/* compute device register field addresses */
 		statusp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANSTATUS * DEVREGLEN));
 		commandp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANCOMMAND * DEVREGLEN));
-		
+
 		/* test device status */
 		stat = termstat(statusp);
 		if (stat == READY || stat == TRANSMITTED) {
 			/* device is available */
-			
+
 			/* print cycle */
 			while (*str != EOS && !error) {
 				cmd = (*str << CHAROFFSET) | PRINTCHR;
@@ -75,14 +75,14 @@ unsigned int termprint(char * str, unsigned int term) {
 				stat = termstat(statusp);
 				while (stat == BUSY)
 					 stat = termstat(statusp);
-				
+
 				/* end of wait */
 				if (stat != TRANSMITTED)
 					error = TRUE;
 				else
 					/* move to next char */
 					str++;
-			} 
+			}
 		}
 		else
 			/* device is not available */
@@ -92,7 +92,7 @@ unsigned int termprint(char * str, unsigned int term) {
 		/* wrong terminal device number */
 		error = TRUE;
 
-	return (!error);		
+	return (!error);
 }
 
 
@@ -113,11 +113,11 @@ void addokbuf(char *strp) {
 void adderrbuf(char *strp) {
 	char *ep = errbuf;
 	char *tstrp = strp;
-	
+
 	while ((*ep++ = *strp++) != '\0');
-	
+
 	termprint(tstrp, 0);
-		
+
 	PANIC();
 }
 
@@ -207,7 +207,7 @@ void main() {
 
 	if (!emptyChild(procp[2]))
 	  adderrbuf("emptyChild: unexpected FALSE   ");
-	
+
 	/* make procp[1] through procp[9] children of procp[0] */
 	addokbuf("Inserting...   \n");
 	for (i = 1; i < 10; i++) {
@@ -240,15 +240,78 @@ void main() {
 
 	if (!emptyChild(procp[0]))
 	    adderrbuf("emptyChild: unexpected FALSE   ");
-	    
+
 	addokbuf("insertChild, removeChild and emptyChild ok   \n");
 	addokbuf("process tree module ok      \n");
 
-	for (i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++)
 		freePcb(procp[i]);
 
 
-	
+	/* check ASL */
+	initASL();
+	addokbuf("Initialized active semaphore list   \n");
+
+	/* check removeBlocked and insertBlocked */
+	addokbuf("insertBlocked test #1 started  \n");
+	for (i = 10; i < MAXPROC; i++) {
+		procp[i] = allocPcb();
+		if (insertBlocked(&sem[i], procp[i]))
+			adderrbuf("insertBlocked(1): unexpected TRUE   ");
+	}
+	addokbuf("insertBlocked test #2 started  \n");
+	for (i = 0; i < 10; i++) {
+		procp[i] = allocPcb();
+		if (insertBlocked(&sem[i], procp[i]))
+			adderrbuf("insertBlocked(2): unexpected TRUE   ");
+	}
+
+	/* check if semaphore descriptors are returned to free list */
+	p = removeBlocked(&sem[11]);
+	if (insertBlocked(&sem[11],p))
+		adderrbuf("removeBlocked: fails to return to free list   ");
+
+	if (insertBlocked(&onesem, procp[9]) == FALSE)
+		adderrbuf("insertBlocked: inserted more than MAXPROC   ");
+
+	addokbuf("removeBlocked test started   \n");
+	for (i = 10; i< MAXPROC; i++) {
+		q = removeBlocked(&sem[i]);
+		if (q == NULL)
+			adderrbuf("removeBlocked: wouldn't remove   ");
+		if (q != procp[i])
+			adderrbuf("removeBlocked: removed wrong element   ");
+		if (insertBlocked(&sem[i-10], q))
+			adderrbuf("insertBlocked(3): unexpected TRUE   ");
+	}
+	if (removeBlocked(&sem[11]) != NULL)
+		adderrbuf("removeBlocked: removed nonexistent blocked proc   ");
+	addokbuf("insertBlocked and removeBlocked ok   \n");
+
+	if (headBlocked(&sem[11]) != NULL)
+		adderrbuf("headBlocked: nonNULL for a nonexistent queue   ");
+	if ((q = headBlocked(&sem[9])) == NULL)
+		adderrbuf("headBlocked(1): NULL for an existent queue   ");
+	if (q != procp[9])
+		adderrbuf("headBlocked(1): wrong process returned   ");
+	p = outBlocked(q);
+	if (p != q)
+		adderrbuf("outBlocked(1): couldn't remove from valid queue   ");
+	q = headBlocked(&sem[9]);
+	if (q == NULL)
+		adderrbuf("headBlocked(2): NULL for an existent queue   ");
+	if (q != procp[19])
+		adderrbuf("headBlocked(2): wrong process returned   ");
+	p = outBlocked(q);
+	if (p != q)
+		adderrbuf("outBlocked(2): couldn't remove from valid queue   ");
+	p = outBlocked(q);
+	if (p != NULL)
+		adderrbuf("outBlocked: removed same process twice.");
+	if (headBlocked(&sem[9]) != NULL)
+		adderrbuf("out/headBlocked: unexpected nonempty queue   ");
+	addokbuf("headBlocked and outBlocked ok   \n");
+	addokbuf("ASL module ok   \n");
+	addokbuf("So Long and Thanks for All the Fish\n");
 
 }
-
