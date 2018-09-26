49b4bb6dcbb7fe0f5c94398aa7d98ccd274ec9bb
Eoin Halligan <ehalligan12@gmail.com>
2018-09-26 03:30:58 -0400

    edited types.h

--- h/types.h
+++ h/types.h
@@ -115,10 +115,10 @@ typedef struct pcb_t {
 
 /* semaphore table entry type */
 typedef struct semd_t {
-	struct semd_PTR s_next;
-	struct semd_PTR s_prev;
+	struct semd_t* s_next;
+	struct semd_t* s_prev;
 	int* s_semAdd;
-	pcb_PTR s_procQ;
+	pcb_t* s_procQ;
 }  semd_t, *semd_PTR;
 
 #endif
