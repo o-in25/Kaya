b829041fc44a0ed3f6dd965ec0da6473b4ddee84
Eoin Halligan <ehalligan12@gmail.com>
2018-09-26 21:43:27 -0400

    fixed freeSemd

--- phase1/asl.c
+++ phase1/asl.c
@@ -309,6 +309,7 @@ int insertBlocked(int* semAdd, pcb_PTR p) {
 		if(openSemd == NULL) {
 			/* no more free semd_t on the free list - out work
 			here is done, so mark the operation as an unsuccessful one */
+			debugA(420);
 			return TRUE;
 		} else {
 			/* arrange the new semd_t so that is in the appropriate place in
