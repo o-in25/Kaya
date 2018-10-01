cd573bb9b7c372cbd04b42c4e5c851fc85d7fd78
Eoin Halligan <ehalligan12@gmail.com>
2018-10-01 12:42:05 -0400

    finished

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -267,7 +267,7 @@ void main() {
 	addokbuf("insertBlocked test #2 started  \n");
 	for (i = 0; i < 10; i++) {
 		procp[i] = allocPcb();
-		debugA(420);
+		debugA(i);
 		if (insertBlocked(&sem[i], procp[i])) {
 			adderrbuf("insertBlocked(2): unexpected TRUE   ");
 		}
