23dcd36270d9f20c35a51016057d536248932f6f
Eoin Halligan <ehalligan12@gmail.com>
2018-10-21 21:51:20 -0400

    working on exceptions

--- h/const.h
+++ h/const.h
@@ -8,17 +8,17 @@
  ****************************************************************************/
 
 /* Maximum process blocks */
-#define MAXPROC  		20
+#define MAXPROC 20
 #define MAXINT 2147483647
-#define MAXSEMALLOC 20
+#define MAXSEMALLOC 49 
 /* Maximum 32-bit signed
 /* Hardware and software constants */
-#define PAGESIZE		4096	/* page size in bytes */
-#define WORDLEN			4		/* word size in bytes */
-#define PTEMAGICNO		0x2A
+#define PAGESIZE 4096	/* page size in bytes */
+#define WORDLEN	4		/* word size in bytes */
+#define PTEMAGICNO 0x2A
 
 
-#define ROMPAGESTART	0x20000000	 /* ROM Reserved Page */
+#define ROMPAGESTART 0x20000000	 /* ROM Reserved Page */
 
 
 
@@ -42,60 +42,72 @@
 #define INTERVALTMR	0x10000020
 #define TIMESCALEADDR	0x10000024
 
+/* syscalls */
+#define CREATEPROCESS 1
+#define TERMINATEPROCESS 2
+#define VERHOGEN 3
+#define PASSEREN 4
+#define SPECIFYEXCEPTIONSTATEVECTOR 5
+#define GETCPUTIME 6
+#define WAITFORCLOCK 7
+#define WAITFORIODEVICE 8
+
+
+
 #define ALLOFF 0
 
 /* utility constants */
-#define	TRUE		1
-#define	FALSE		0
-#define ON              1
-#define OFF             0
-#define HIDDEN		static
-#define EOS		'\0'
+#define	TRUE 1
+#define	FALSE 0
+#define ON 1
+#define OFF 0
+#define HIDDEN static
+#define EOS	'\0'
 
 #define NULL ((void *)0xFFFFFFFF)
 
 
 /* vectors number and type */
-#define VECTSNUM	4
+#define VECTSNUM 4
 
-#define TLBTRAP		0
-#define PROGTRAP	1
-#define SYSTRAP		2
+#define TLBTRAP	0
+#define PROGTRAP 1
+#define SYSTRAP	2
 
-#define TRAPTYPES	3
+#define TRAPTYPES 3
 
 
 /* device interrupts */
-#define DISKINT		3
-#define TAPEINT 	4
-#define NETWINT 	5
-#define PRNTINT 	6
-#define TERMINT		7
+#define DISKINT	3
+#define TAPEINT 4
+#define NETWINT 5
+#define PRNTINT 6
+#define TERMINT	7
 
-#define DEVREGLEN	4	/* device register field length in bytes & regs per dev */
-#define DEVREGSIZE	16 	/* device register size in bytes */
+#define DEVREGLEN 4	/* device register field length in bytes & regs per dev */
+#define DEVREGSIZE 16 	/* device register size in bytes */
 
 /* device register field number for non-terminal devices */
-#define STATUS		0
-#define COMMAND		1
-#define DATA0		2
-#define DATA1		3
+#define STATUS 0
+#define COMMAND	1
+#define DATA0 2
+#define DATA1 3
 
 /* device register field number for terminal devices */
-#define RECVSTATUS      0
-#define RECVCOMMAND     1
-#define TRANSTATUS      2
-#define TRANCOMMAND     3
+#define RECVSTATUS 0
+#define RECVCOMMAND 1
+#define TRANSTATUS 2
+#define TRANCOMMAND 3
 
 
 /* device common STATUS codes */
 #define UNINSTALLED	0
-#define READY		1
-#define BUSY		3
+#define READY 1
+#define BUSY 3
 
 /* device common COMMAND codes */
-#define RESET		0
-#define ACK		1
+#define RESET 0
+#define ACK	1
 
 /* operations */
 #define	MIN(A,B)	((A) < (B) ? A : B)
---
+++ phase2/exceptions.c
@@ -0,0 +1,34 @@
+#include "../h/const.h"
+#include "../h/types.h"
+
+ void syscallHandler() {
+    int callNumber = 0; /* TODO: properly assign the number and handle case  */
+    switch(callNumber) {
+    case WAITFORIODEVICE:
+        return NULL;
+    case WAITFORCLOCK:
+        return NULL;
+    case GETCPUTIME:
+        return NULL;
+    case SPECIFYEXCEPTIONSTATEVECTOR:
+        return NULL;
+    case PASSEREN:
+        return NULL;
+    case VERHOGEN:
+        return NULL;
+    case TERMINATEPROCESS:
+        return NULL;
+    case CREATEPROCESS:
+        return NULL;
+    }
+ }
+
+ void programTrapHandler() {
+
+ }
+
+ void tableHandler() {
+
+ }
+
+ 
