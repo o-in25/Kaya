843e77cd12f7132ddc9538f679766b17d66e404f
Halligan <ehalliga@ws.wsfgrp.net>
2018-10-23 16:38:52 -0400

    working on scheduler */

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -7,23 +7,44 @@
 
 void invokeScheduler() {
 
-    if(processCount < 1) {
 
+    pcb_PTR currentProcess = removeProcQ(&(readyQueue));
+    /*  */
+    if(!(emptyProcQ(&(readyQueue)))) {
+        /* TODO: load the state */
+        LDST(&(currentProcess->p_state));
     } else {
-        if(softBlockedCount < 1) {
-            /* invoke privilaged ROM instruction */
-            PANIC();
-        } else {
-            WAIT();
-        }
+        /* there is nothing on the ready queue to be scheduled. we must check for special cases first, what if there are no 
+        processes left in the system - that is, the process count is less than 1? If this is the case, we invoke 
+        the privilaged ROM command HALT. secondly, what if there are running processes in the system, but the ready 
+        queue is empty? there are possible cases for this, despite seeming unintuitive. first, if there is the possibility 
+        that a process is waiting on I/O to complete. If this is the case, the softblocked out will be > 0. Since these 
+        processes are gaurenteed to cause an interrupt, we simply have to wait for them to finish. appropriately, this is done
+        with the privaleged ROM command WAIT. finally, if neither of these are the case there is an error. here, there are no 
+        jobs in the ready queue - but none of them are waiting for I/O. If this is the case, the ROM command PANIC is invoked  
+        and we close up shop */
+        /* case 1: the ready queue is empty, but there are soft blocked processes waiting to return */
+        if(processCount == 0) { /* case 1 */
+            /* our work here is done. there are no jobs in the ready queue
+            and we have no processes running */
+            HALT();
+        } else if(processCount > 0) { /* case 2 */
+            /* no current process, since the we have no process counts */
+            currentProcess = NULL;
+            /* now, we have 2 subcases. there isn't anything on the ready queue, but we have at least one active process
+            so why is this? we are either softblocked and waiting on I/O - in which case all is good, we just wait it out.
+            but if we are not waiting on I/O there's nothing on the ready queue, AND we have a processes lingering,
+            we panic */
+            if(softblockedCount > 0) {
+                /* all is good - waiting on I/O to finish up */
+                WAIT();
+            } else if(softblockedCount == 0) {
+                /* kernel panic. we have nothing on the ready queue, we have a process lingering - but it's not
+                I/O - time to panic */
+                PANIC();
+            }
+        } 
     }
-    currentProcess = removeProcQ(&(readyQueue));
-
-
-
-
-    /* privlaged ROM instruction */
-    LDST();
 }
 
 void schedule() {
