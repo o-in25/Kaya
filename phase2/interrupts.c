5d96ae4f70ba89d8be3f7060af93fd0ec3049b4a
Eoin Halligan <ehalligan12@gmail.com>
2018-10-31 18:09:40 -0400

    going to try and compile

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -81,7 +81,7 @@ static void terminalHandler(device_PTR devAddrBase, int* status) {
         by providing the acknowledge bit */
         (*devAddrBase).t_transm_command = ACK;
         /* set the status to be a transmit status */
-        (*status) = (*devAddrBase).t_transm_status;
+        (*status) = (unsigned int*) (*devAddrBase).t_transm_status;
     } else {
         /* acknowledge that the command is a recieve command 
         by providing the acknowledge bit */
