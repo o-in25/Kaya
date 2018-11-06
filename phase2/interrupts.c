cba79e5153aa678666e5577bdd67f93266f0c5b9
Eoin Halligan <ehalligan12@gmail.com>
2018-11-06 12:20:18 -0500

    tweaked getDeviceNumber

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -34,7 +34,8 @@ static unsigned int getDeviceNumber(int lineNumber) {
     /* get the address of the device bit map. Per secion 5.2.4 of pops, the 
     physical address of the bit map is 0x1000003C. When bit i is in word j is 
     set to one then device i attached to interrupt line j + 3 */
-    const unsigned int* deviceBitMap = (unsigned int*) DEVREGLEN + (lineNumber - NOSEM) * WORDLEN;
+    devregarea_PTR devReg = (devregarea_PTR) RAMBASEADDR;
+    const unsigned int* deviceBitMap = devReg->interrupt_dev[lineNumber - NOSEM];
     debugA(991);
 
     /* start at the first device */
