62445bcafef747fbdf202b43d11c75c8de213b13
Eoin Halligan <ehalligan12@gmail.com>
2018-11-08 23:23:15 -0500

    working on timing

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -9,45 +9,38 @@
 cpu_t startTOD;
 cpu_t currentTOD;
 extern void invokeScheduler() {
-    if (currentProcess != NULL) {
-        STCK(currentTOD);
-        currentProcess->p_time = currentProcess->p_time + (currentTOD - startTOD);
-    }
-    if(!emptyProcQ(readyQueue)) {
-        currentProcess = removeProcQ(&(readyQueue));
-        STCK(startTOD);
-        setTIMER(QUANTUM);
-        /* DEBUG NOTES: got to here before printing p */
-        contextSwitch(&(currentProcess->p_state));
-    } else {
-
-        if (processCount == 0)
-        { /* case 1 */
-            /* our work here is done. there are no jobs in the ready queue
+    if(emptyProcQ(readyQueue)) {
+        currentProcess = NULL;
+        if(processCount == 0) { /* case 1 */
+                /* our work here is done. there are no jobs in the ready queue
                 and we have no processes running */
-            HALT();
+                HALT();
         }
-        if (processCount > 0)
-        {
+        if(processCount > 0) {
             /* no current process, since the we have no process counts */
-            currentProcess = NULL;
             /* now, we have 2 subcases. there isn't anything on the ready queue, but we have at least one active process
             so why is this? we are either softblocked and waiting on I/O - in which case all is good, we just wait it out.
             but if we are not waiting on I/O there's nothing on the ready queue, AND we have a processes lingering,
             we panic */
-            if (softBlockedCount == 0)
-            {
+            if(softBlockedCount == 0) {
                 /* all is good - waiting on I/O to finish up */
                 PANIC();
-            }
-            else if (softBlockedCount > 0)
-            {
+            } else if(softBlockedCount > 0) {
                 /* kernel panic. we have nothing on the ready queue, we have a process lingering - but it's not
                 I/O - time to panic */
                 setSTATUS(getSTATUS() | ALLOFF | INTERRUPTSON | IEc | IM);
                 WAIT();
             }
         }
-
+    } else {
+        if (currentProcess != NULL) {
+            STCK(currentTOD);
+            currentProcess->p_time = currentProcess->p_time + (currentTOD - startTOD);
+        }
+        currentProcess = removeProcQ(&(readyQueue));
+        STCK(startTOD);
+        setTIMER(QUANTUM);
+        /* DEBUG NOTES: got to here before printing p */
+        contextSwitch(&(currentProcess->p_state));
     }
 }
