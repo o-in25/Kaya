eb584351ec8f094c9eed216d5c892b21f11c6a7f
Eoin Halligan <ehalligan12@gmail.com>
2018-11-08 23:17:50 -0500

    working on timing

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -9,7 +9,21 @@
 cpu_t startTOD;
 cpu_t currentTOD;
 extern void invokeScheduler() {
-    if(emptyProcQ(readyQueue)) {
+    if (currentProcess != NULL)
+    {
+        STCK(currentTOD);
+        currentProcess->p_time = currentProcess->p_time + (currentTOD - startTOD);
+    }
+    else if(!emptyProcQ(readyQueue)) {
+        currentProcess = removeProcQ(&(readyQueue));
+        STCK(startTOD);
+        setTIMER(QUANTUM);
+        /* DEBUG NOTES: got to here before printing p */
+        contextSwitch(&(currentProcess->p_state));
+    }
+    else {
+        currentProcess = NULL;
+
         if(processCount == 0) { /* case 1 */
                 /* our work here is done. there are no jobs in the ready queue
                 and we have no processes running */
@@ -17,7 +31,6 @@ extern void invokeScheduler() {
         }
         if(processCount > 0) {
             /* no current process, since the we have no process counts */
-            currentProcess = NULL;
             /* now, we have 2 subcases. there isn't anything on the ready queue, but we have at least one active process
             so why is this? we are either softblocked and waiting on I/O - in which case all is good, we just wait it out.
             but if we are not waiting on I/O there's nothing on the ready queue, AND we have a processes lingering,
@@ -34,10 +47,6 @@ extern void invokeScheduler() {
         }
     } else {
        
-        currentProcess = removeProcQ(&(readyQueue));
-        STCK(startTOD);
-        setTIMER(QUANTUM);
-        /* DEBUG NOTES: got to here before printing p */
-        contextSwitch(&(currentProcess->p_state));
+      
     }
 }
