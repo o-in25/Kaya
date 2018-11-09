dba1c51aa0caa5bc46de732e2ec23b956748644a
Eoin Halligan <ehalligan12@gmail.com>
2018-11-08 23:24:55 -0500

    working on timing

--- phase2/initial.c
+++ phase2/initial.c
@@ -114,7 +114,6 @@ int main() {
     /* insert the newly allocated process into the ready queue */
     insertProcQ(&(readyQueue), currentProcess);
     /* its in the queue */
-    currentProcess = NULL;
     /* call the scheduler */
     LDIT(INTERVAL);
     invokeScheduler();
