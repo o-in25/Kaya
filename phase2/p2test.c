764207c105662e325f685e612d3e1f608f14ae95
Eoin Halligan <ehalligan12@gmail.com>
2018-11-09 12:48:39 -0500

    working on p3 started

--- phase2/p2test.c
+++ phase2/p2test.c
@@ -337,12 +337,12 @@ void p2() {
     debugthing (3);
     SYSCALL(VERHOGEN, (int)&endp2, 0, 0);                /* V(endp2)     */
     debugthing(4);
+    halt(69); /* PANIC!           */
     SYSCALL(TERMINATETHREAD, 0, 0, 0);            /* terminate p2 */
     
     /* just did a SYS2, so should not get to this point */
     print("error: p2 didn't terminate\n");
     PANIC();  
-    halt(69);                  /* PANIC!           */
 }
 
 
