d7565621c8a677dc3ea1c498955deb84670c8a59
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2018-11-11 22:02:25 -0500

    its hard to know your going the wrong direction when your looking at the wrong place

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -382,7 +382,7 @@ static void delegateSyscall(int callNumber, state_PTR caller) {
        state_PTR programTrapOldArea = (state_PTR) PRGMTRAPOLDAREA;
             /* copy the state */
             copyState(caller, programTrapOldArea);
-            programTrapOldArea->s_cause = programTrapOldArea->s_cause | (RESERVED << 2);
+            programTrapOldArea->s_cause = (programTrapOldArea->s_cause & (~(unsigned int) 60) | (RESERVED << 2);
             /* call a program trap */
             programTrapHandler();
     }  
