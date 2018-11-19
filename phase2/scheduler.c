aa9a51463caf2ea373a9b24284d24c8d8cbda283
Eoin Halligan <ehalligan12@gmail.com>
2018-11-19 11:38:13 -0500

    refactoring interrupts

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -12,21 +12,12 @@ extern void invokeScheduler() {
     if(emptyProcQ(readyQueue)) {
         currentProcess = NULL;
         if(processCount == 0) { /* case 1 */
-                /* our work here is done. there are no jobs in the ready queue
-                and we have no processes running */
                 HALT();
         }
         if(processCount > 0) {
-            /* no current process, since the we have no process counts */
-            /* now, we have 2 subcases. there isn't anything on the ready queue, but we have at least one active process
-            so why is this? we are either softblocked and waiting on I/O - in which case all is good, we just wait it out.
-            but if we are not waiting on I/O there's nothing on the ready queue, AND we have a processes lingering,
-            we panic */
             if(softBlockedCount == 0) {
-                /* all is good - waiting on I/O to finish up */
+                PANIC();
             } else if(softBlockedCount > 0) {
-                /* kernel panic. we have nothing on the ready queue, we have a process lingering - but it's not
-                I/O - time to panic */
                 setSTATUS(getSTATUS() | ALLOFF | INTERRUPTSON | IEc | IM);
                 WAIT();
             }
@@ -36,11 +27,9 @@ extern void invokeScheduler() {
             STCK(currentTOD);
             currentProcess->p_time = currentProcess->p_time + (currentTOD - startTOD);
         }
-        if(!emptyProcQ(readyQueue)) {
-            currentProcess = removeProcQ(&(readyQueue));
-            STCK(startTOD);
-            setTIMER(QUANTUM);
-            contextSwitch(&(currentProcess->p_state));
-        }
+        currentProcess = removeProcQ(&(readyQueue));
+        STCK(startTOD);
+        setTIMER(QUANTUM);
+        contextSwitch(&(currentProcess->p_state));
     }
 }
