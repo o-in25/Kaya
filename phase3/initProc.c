b69e49fcc583e7c540daa698377efff327704971
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2018-12-05 13:33:23 -0500

    video 2 implemented

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -2,12 +2,29 @@
 #include "../h/types.h"
 #include "../e/pager.e"
 
-/* The master semaphore */
-int masterSemaphore = 0;
-/* One semaphore for each device */
+
+/* Page table for kUseg3 */
+/* 8 kUseg2 page tables */
+/* kSegOS page table */
+
+/* master semaphore */
+int* masterSemaphore;
+
+/*device semaphores */
 int mutexSemaphores[MAXSEMALLOC];
 
+/* framepool data structure */
+frame swappool[MAXPROCESS*2];
 
+/* user process data structure */
+user userProcess[MAXPROCESS];
+
+/*initialization for elements to be put into where we start */
+for (int i = 0; i < MAXSEMALLOC; i++){
+	semdTable[i] = 1;
+}
+*masterSemaphore = 0;
+/*need to initialize the semaphores for userProcess to 0 */ 
 
 
 void test() {
