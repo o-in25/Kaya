fb9c6a5f6fcbfbd97757d5d1680c14cecb026888
Eoin Halligan <ehalligan12@gmail.com>
2018-12-06 19:04:48 -0500

    refactoring

--- phase2/initial.c
+++ phase2/initial.c
@@ -19,6 +19,7 @@
 #include "../e/interrupts.e"
 #include "../e/exceptions.e"
 #include "../e/scheduler.e"
+#include "../e/p2test.e"
 #include "/usr/local/include/umps2/umps/libumps.e"
 
 /* globals */
