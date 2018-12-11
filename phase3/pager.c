e6c4eb87c878c964772b2235b508c009e3f41e6c
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2018-12-11 18:59:34 -0500

    working through tlbhandler

--- phase3/pager.c
+++ phase3/pager.c
@@ -2,10 +2,22 @@ void test() {
     
 }
 
+int lastFrame
+
+void progTrapHandler (){
+    /* SYS18 */
+}
+
+/* just returns an increment on the last frame mod to simulate an incremental choice */
+int nextFrame (){
+    if (!lastFrame){
+        lastFrame = 0;
+    }
+    lastFrame = lastFrame + 1;
+    return (lastFrame % (8*2));
+}
 
 void TLBhandler (){
-    
-    memaddr address;
     devregarea_t* devReg = (devregarea_t*) RAMBASEADDR;
     memaddr RAMTOP = devReg->rambase + devReg->ramsize;
     memaddr swappoolstart = RAMTOP - (2 * PAGESIZE) - (SWAPSIZE * PAGESIZE);
@@ -17,7 +29,7 @@ void TLBhandler (){
     
     /* why are we here */
     /*examine oldmem cause register */
-    state_t* oldState = (state_t*) &(uProcesses[missing_ASID - 1]./*however we do olt trap area for tlb */)
+    state_t* oldState = (state_t*) &(uProcesses[missing_ASID - 1]./*however we do old trap area for tlb */)
     int cause = (oldState->s_cause & /* bits 2-6 */) >> 2;
     /* if TLB Invalid then SYS18 */
     /* 2 and 3 only valid TLB causes (pg 16 in yellow book) */
@@ -35,14 +47,29 @@ void TLBhandler (){
     
     /* if missing page (mp) is from KUseg3, check if the page is still missing */
     /* check the KUseg3 page table enty's valid bit */
-    
-    /* if no longer missing, release mutex and return control */
-    /* LDST oldMem */
+    if (/*determine if it is from kusegos and if it is now valid, if both are true then you execute the following */){
+        /* if no longer missing, release mutex and return control */
+        /* LDST oldMem */
+        SYSCALL (VERHOGEN, (int) &swapSemaphore, 0, 0);
+        LDST (oldState);
+    }
     
     /* pick a frame to use */
+    int frame = nextFrame ();
+    memaddr address = SWAPPOOLSTART + (frameNumber * PAGESIZE);
+    
     /* if the frame is currently occupied */
-    /* turn the valid bit off in the page table of the current frames occupent */
-    /* deal with the TLB cache consistency */
+    if (pool[frame].ASID != -1){
+        
+        /* disable interupts */
+        
+        /* turn the valid bit off in the page table of the current frames occupent */
+        pool[frame].pageTableEntry->entryLO = pool[frame].pageTableEntry->entryLO & INVALID;
+        /* deal with the TLB cache consistency */
+        /* by clearing the TLB */
+        TLBCLR ();
+        /* enable interupts */
+    }
     /* write current frames contents on the backing store */
     /* read missing page into selected frame */
     /*update the swapool data structure */
