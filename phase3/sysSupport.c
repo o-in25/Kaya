bbd2d6afa40405aeb8ad88dd88fca2ec2a38d437
Eoin Halligan <ehalligan12@gmail.com>
2018-12-11 23:28:23 -0500

    working on diskIO

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -1,16 +1,30 @@
 #include "../h/const.h"
 #include "../h/types.h"
 
+/* used to gain mutual exclusion on a passed in semaphore or 
+release mutal exclusion so that a process can be done atomically. Here,
+TRUE is to gain mutal exclusion and FALSE is to release it */
+void mutex(int flag, int *semaphore) {
+    /* are we gaining control? */
+    if(flag) {
+        SYSCALL(PASSEREN, (int)semaphore, EMPTY, EMPTY);
+    } else {
+        /* no - we are releasing it */
+        SYSCALL(VERHOGEN, (int)semaphore, EMPTY, EMPTY);
+    }
+} 
+
 /* read in the uproc's .data and .text from the tape */
 void diskOperation(int diskInformation[], int *semaphore, device_PTR diskDevice) {
     /* initialize the disk with the disk number */
     diskDevice = (device_PTR) diskDevice + (diskInformation[DISKNUM] * DEVREGSIZE);
-    /* gain control */
-    mutex(TRUE, semaphore);
     /* save the status before we turn everything off */
     int oldStatus = getSTATUS();
+    /* gain control */
+    mutex(TRUE, semaphore);
     /* turn off interrupts */
     setSTATUS(ALLOFF);
+
     diskDevice->d_command = ALLOFF;
     int status = SYSCALL(WAITIO, DISKINT, diskInformation[DISKNUM], EMPTY);
     /* return to how we were */
@@ -26,15 +40,3 @@ void diskOperation(int diskInformation[], int *semaphore, device_PTR diskDevice)
     mutex(FALSE, semaphore);
 }
 
-/* used to gain mutual exclusion on a passed in semaphore or 
-release mutal exclusion so that a process can be done atomically. Here,
-TRUE is to gain mutal exclusion and FALSE is to release it */
-void mutex(int flag, int *semaphore) {
-    /* are we gaining control? */
-    if (flag) {
-        SYSCALL(PASSEREN, (int)semaphore, EMPTY, EMPTY);
-    } else {
-        /* no - we are releasing it */
-        SYSCALL(VERHOGEN, (int)semaphore, EMPTY, EMPTY);
-    }
-}
