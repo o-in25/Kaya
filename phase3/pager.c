895777b7875b95b665826e426f52f3ca47c3bdd8
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2018-12-14 11:38:39 -0500

    tlbhandler complete. Still need sys18 for prog handler and a bit in tlbhandler,but otherwise I think this file is done for now

--- phase3/pager.c
+++ phase3/pager.c
@@ -4,6 +4,71 @@
 #include "../e/initProc.e"
 
 
+/* disables interrupts on request */
+void disableInterrupts() {
+    int status = getSTATUS();
+    status = (status & 0xFFFFFFFE);
+    setSTATUS(status);
+}
+
+/* enables interrupts on request */
+void enableInterrupts() {
+    int status = getSTATUS();
+    status = (status & 0x1);
+    setSTATUS(status);
+}
+
+
+/* readBacking reads in data from the backing store using the givin cylinder, sector, head and address to write to */
+void readBacking (int cylinder, int sector, int head, memaddr address){
+    SYSCALL(PASSEREN, (int)&disk0Semaphore, 0, 0);
+    
+    devregarea_t* devReg = (devregarea_t *) RAMBASEADDR;
+    device_t* disk = &(devReg->devreg[0])
+    unsigned int status;
+    
+    disableInterrupts();
+    disk->d_command = (cylinder << 8) | 2;
+    status = SYSCALL (WAITFORIO, DISKINT, BACKINGSTORE, 0);
+    enableInterrupts();
+    
+    if (status == 1){
+        disableInterrupts();
+        disk->d_data0 = address;
+        disk->d_command = (head << 16) | ((sector - 1) << 8) |  4;
+        
+        status = SYSCALL (WAITFORIO, DISKINT, BACKINGSTORE, 0);
+        enableInterrupts();
+    }
+    
+    SYSCALL VERHOGEN, (int)&disk0Semaphore, 0, 0);
+}
+
+/* write backing writes to the backing store given a cylinder, sector, head and memory address to write from */
+void writeBacking (int cylinder, int sector, int head, memaddr address){
+    SYSCALL(PASSEREN, (int)&disk0Semaphore, 0, 0);
+    
+    devregarea_t* devReg = (devregarea_t *) RAMBASEADDR;
+    device_t* disk = &(devReg->devreg[0])
+    unsigned int status;
+    
+    disableInterrupts();
+    disk->d_command = (cylinder << 8) | 2;
+    status = SYSCALL (WAITFORIO, DISKINT, BACKINGSTORE, 0);
+    enableInterrupts();
+    
+    if (status == 1){
+        disableInterrupts();
+        disk->d_data0 = address;
+        disk->d_command = (head << 16) | ((sector - 1) << 8) | 3;
+        
+        status = SYSCALL (WAITFORIO, DISKINT, BACKINGSTORE, 0);
+        enableInterrupts();
+    }
+    
+    SYSCALL VERHOGEN, (int)&disk0Semaphore, 0, 0);
+}
+
 void pager() {
     
     
@@ -13,6 +78,7 @@ void pager() {
 void test() {
     
 }
+
 
 
 
@@ -42,11 +108,11 @@ void TLBhandler (){
     /* who am I? */
     /* get current processid in ASID register */
     /* this is needed as the index into the phase3 global structure */
-    int missing_ASID = ((getENTRYHI() & 0) >> ASIDMASK);
+    int missing_ASID = ((getENTRYHI() & 0x3FFFF000) >> ASIDMASK);
     
     /* why are we here */
     /*examine oldmem cause register */
-    state_PTR oldState = (state_PTR) & (uProcesses[missing_ASID - 1] /*however we do old trap area for tlb */);
+    state_PTR oldState = (state_PTR) & (uProcesses[missing_ASID - 1].Told_trap[2]);
     int cause = (oldState->s_cause & 0 /* bits 2-6 */) >> 2;
     /* if TLB Invalid then SYS18 */
     /* 2 and 3 only valid TLB causes (pg 16 in yellow book) */
@@ -63,37 +129,58 @@ void TLBhandler (){
     SYSCALL (PASSEREN, (int) &swapSemaphore, 0, 0);
     
     /* if missing page (mp) is from KUseg3, check if the page is still missing */
-    /* check the KUseg3 page table enty's valid bit */
-    if (/*determine if it is from kusegos and if it is now valid, if both are true then you execute the following */){
+    /* check the KUseg3 page table entry's valid bit */
+    if (segmentNumber == 3 && /*need to check if valid bit on */) != 0){
         /* if no longer missing, release mutex and return control */
-        /* LDST oldMem */
         SYSCALL (VERHOGEN, (int) &swapSemaphore, 0, 0);
         LDST (oldState);
     }
     
     /* pick a frame to use */
     int frame = nextFrame ();
-    memaddr address = SWAPPOOLSTART + (frameNumber * PAGESIZE);
+    memaddr address = SWAPPOOLSTART + (frame * PAGESIZE);
     
     /* if the frame is currently occupied */
     if (pool[frame].ASID != -1){
         
-        /* disable interupts */
-        
+        disableInterrupts();
         /* turn the valid bit off in the page table of the current frames occupent */
         pool[frame].pageTableEntry->entryLO = pool[frame].pageTableEntry->entryLO & INVALID;
         /* deal with the TLB cache consistency */
         /* by clearing the TLB */
         TLBCLR ();
-        /* enable interupts */
+        enableInterrupts();
+        
+        int squatterASID = pool[frame].ASID;
+        int squatterPageNum = pool[frame].pageNumber;
+        /* write current frames contents on the backing store */
+        writeBacking (squatterPageNum, squatterASID, 0, address);
+        
     }
-    /* write current frames contents on the backing store */
+    
     /* read missing page into selected frame */
+    readBacking(pageNumber, missing_ASID, 0, address);
+    disableInterrupts();
     /*update the swapool data structure */
+    pool[frame].ASID = missing_ASID;
+    pool[frame].segmentNumber = segmentNumber;
+    pool[frame].pageNumber = pageNumber;
     /* update missing pages page table entry: frame and valid bit */
+    if (segmentNumber == 3) {
+        pool[frame].pageTableEntry = &(KUseg3.pteTable[pageNumber]);
+        pool[frame].pageTableEntry->entryLO = address | VALID | DIRTY |GLOBAL;
+    } else {
+        pool[frame].pageTableEntry = &(uProcesses[missing_ASID].tp_pte.pteTable[pageNumber]);
+        pool[frame].pageTableEntry->entryLO = address | VALID | DIRTY |GLOBAL;
+    }
     /* deal with the cache consitency */
+    TLBCLR();
+    enableInterrupts();
     
     /*release mutex and return control to process */
+    SYSCALL(VERHOGEN, (int)&swapSemaphore, 0, 0);
+    
+    LDST(oldState);
 }
 
 
