bdff6376a859fb824abc763e6ad8022eb9f05848
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2018-12-16 17:43:17 -0500

    syscalls 10, 17 and 18 implemented

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -38,7 +38,7 @@ static void delegateUSyscall(state_PTR state) {
             writeToPrinter(state);
             break;
         case GET_TOD:
-            getTOD();
+            getTOD(state);
             break;
         case TERMINATE:
             terminateUProcess();
@@ -86,7 +86,7 @@ static void readFromTerminal(state_PTR state) {
         if ((status & 0xFF) != 5){
             PANIC();
         }
-        address++
+        address++;
     }
     
     /* put the ammount that were  written into v0 */
@@ -97,19 +97,51 @@ static void readFromTerminal(state_PTR state) {
 }
 
 static void writeToTerminal(state_PTR state) {
-
+    char *address = state->s_a1;
+    char *length = state->s_a2;
+    int ASID = ((getENTRYHI() & 0x00000FC0) >> ASIDMASK);
+    int deviceNumber = 32 + (ASID - 1);
+    devregarea_PTR devReg = (devregarea_PTR) RAMBASEADDR;
+    device_PTR terminal = &(devReg->devreg[deviceNumber]);
+    
+    /* call dibs */
+    SYSCALL (PASSEREN, (int)&mutexSemaphores[40 + (ASID - 1)], 0, 0);
+    
+    unsigned int status;
+    /* loop to write the string */
+    int i = 0;
+    while (i < length){
+        
+        disableInterrupts();
+        /* set the command and call a wait for io */
+        terminal->t_transm_command = 2 | (((unsigned int) *address) << 8);
+        status = SYSCALL(WAITIO, 7, (ASID - 1), 0);
+        enableInterrupts();
+        
+        /* check for error */
+        if ((status & 0xFF) != 5){
+            PANIC();
+        }
+        
+        /* set up for the next character */
+        address++;
+        i++;
+    }
+    
+    /* return the mutex */
+    SYSCALL (VERHOGEN, (int)mutexSemaphores[40 + (ASID - 1)], 0, 0);
 }
 
 static void vVerhogen() {
-
+    /* dont think we need this one for now */
 }
 
 static void vPasseren() {
-
+    /* dont think we need this one for now */
 }
 
 static void delay() {
-
+    /* dont think we need this one for now */
 }
 
 static void diskPut() {
@@ -144,12 +176,42 @@ static void writeToPrinter(state_PTR state) {
 
 }
 
-static void getTOD() {
-
+static void getTOD(state_PTR state) {
+    /* seems too easy, please double check my work */
+    cpu_t TOD;
+    STCK (TOD);
+    state->s_v0 = TOD;
+    LDST (state);
 }
 
 static void terminateUProcess() {
-
+    int ASID = ((getENTRYHI() & 0x00000FC0) >> ASIDMASK);
+    
+    /* call dibs */
+    SYSCALL(PASSEREN, (int)&swapSemaphore, 0, 0);
+    
+    disableInterrupts();
+    
+    /* set page table and the swap pool entries to invalid */
+    int touched = FALSE; /* used to know if we have to clear the tlb or not */
+    for (int i = 0; i < 16; i++){
+        if(pool[i].ASID == ASID){
+            pool[i].pageTableEntry->entryLO = (pool[i].pageTableEntry->entryLO | INVALID);
+            pool[i].ASID = -1;
+            touched = TRUE;
+        }
+    }
+    if (touched) { /* we handle any problems with the tlb in the most elegant of ways */
+        TLBCLR();
+    }
+    enableInterrupts ();
+    
+    /* we no longer need the semaphore */
+    SYSCALL(VERHOGEN, (int)&swapSemaphore, 0, 0);
+    SYSCALL(VERHOGEN, (int)&masterSemaphore, 0, 0);
+    
+    /* and to finish it off we add a dash of genocide */
+    SYSCALL (TERMINATEPROCESS, 0, 0, 0);
 }
 
 
@@ -159,10 +221,10 @@ TRUE is to gain mutal exclusion and FALSE is to release it */
 void mutex(int flag, int *semaphore) {
     /* are we gaining control? */
     if(flag) {
-        SYSCALL(PASSEREN, (int) semaphore, EMPTY, EMPTY);
+        SYSCALL(PASSEREN, (int) semaphore, 0, 0);
     } else {
         /* no - we are releasing it */
-        SYSCALL(VERHOGEN, (int) semaphore, EMPTY, EMPTY);
+        SYSCALL(VERHOGEN, (int) semaphore, 0, 0);
     }
 } 
 
