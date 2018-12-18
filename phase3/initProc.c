cf709dafcfe643baf3bf57eaa5a954e97075f61f
Eoin Halligan <ehalligan12@gmail.com>
2018-12-17 23:33:58 -0500

    debugging

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -101,15 +101,12 @@ state_PTR prepareProcessorState(int flag, int* index) {
 		processorState->s_asid = getENTRYHI();
 	} else {
 		/* this is for variable initialization i.e. step 1 */
-				debugger(10);
-		processorState->s_asid = ((*(index)) << ASIDMASK);
-						debugger(11);
-
 		processorState->s_status = ALLOFF | IEc | IM | TE;
 		/* the init handler */
 		processorState->s_t9 = (memaddr) initUProc;
 		/* TODO: set up pc */
 		processorState->s_sp = (memaddr) initUProc;
+		debugger(10);
 	}
 	return processorState;
 }
@@ -170,7 +167,8 @@ void test() {
 		uProcesses[i - 1].Tp_pte.pteTable[KUSEGPTESIZE-1].entryHI = (BSDGMT  << VPNMASK) | (i << ASIDMASK);
 		debugger(8);
 		/* add a new processor state, per the student guide */
-		state_PTR processorState = prepareProcessorState(FALSE, &(i));
+		state_PTR processorState = prepareProcessorState(FALSE, 0;
+		processorState->s_asid = (i << ASIDMASK);
 		debugger(9);
 		/* set the semaphore */
 		uProcesses[i - 1].Tp_sem = 0;
